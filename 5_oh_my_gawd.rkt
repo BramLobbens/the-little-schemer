504b4a24813f6e321f6d6a302c03b5a0fd0e93a0
BramLobbens <lobbensbram@gmail.com>
2025-06-23 19:50:33 +0200

    add insertR*

--- 5_oh_my_gawd.rkt
+++ 5_oh_my_gawd.rkt
@@ -18,3 +18,18 @@
       [else (cons (rember* a (car lst)) (rember* a (cdr lst)))])))
 
 (rember* 'cup (list (list 'coffee) 'cup (list (list 'tea) 'cup) (list 'and (list 'hick)) 'cup))
+
+;;insertR*
+(define insertR*
+  (lambda (new_ old l)
+    (cond
+      [(null? l) (quote ())]
+      [(atom? (car l))
+       (cond
+         [(eq? (car l) old)
+          (cons old (cons new_ (insertR* new_ old (cdr l))))]
+         [else (cons (car l) (insertR* new_ old (cdr l)))])]
+      [else (cons (insertR* new_ old (car l)) (insertR* new_ old (cdr l)))])))
+
+(insertR* 'roast 'chuck
+  (list (list 'how 'much (list 'wood)) 'could (list (list 'a (list 'wood) 'chuck)) (list (list (list 'chuck))) (list 'if (list 'a) (list (list 'wood 'chuck))) 'could 'chuck 'wood))
